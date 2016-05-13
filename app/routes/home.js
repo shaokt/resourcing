5aa85a131bb6adeb428b75fb20805137b82e0daa
shaokt <shaorobics@gmail.com>
2016-05-13 14:53:02 -0400

    test for loading manager specific files

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,10 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    id: null,
+
+    beforeModel: function(transition){
+        this.id = transition.queryParams.id
+        console.log(this.id);
+    },
+
     model() {
         return Ember.RSVP.hash({
             resource: this.get('store').findAll('resource'),
             assignment: this.get('store').findAll('assignment')
+            //resource: this.get('store').find('user', 'Kristin')
+            //resource: this.get('store').findAll('user', {id:'Kristin'}),
         });
     },
 
