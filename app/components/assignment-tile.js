2d12467ae5a3a8f9c8c0b017236407c91ad2ec83
shaokt <shaorobics@gmail.com>
2016-06-15 14:10:24 -0400

    suppress assignment tiles in header
    - if in assignment view, do not render tiles in header
    - render tiles per each row to avoid double the work

--- app/components/assignment-tile.js
+++ app/components/assignment-tile.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     tagName: '',
+    omit:false, // do not render the tiles in the header if set to true
 
     /*
     * in resource view, the model is the assignment
@@ -10,7 +11,10 @@ export default Ember.Component.extend({
     init: function(){
         this._super();
         if(this.get('model.assignment')){
-            this.set('model', this.get('model.assignment'))
+            this.set('model', this.get('model.assignment'));
+        }
+        else {  // do not render the tiles in the header...they will render on each assignment row
+            this.set('omit', true);
         }
     },
 
