b13894ba2b619898cda5ee045adec4fa8ec7143e
shaokt <shaorobics@gmail.com>
2016-06-24 15:26:07 -0400

    change calendars if there are people on this assignment
    - if people are assigned, changed to vacation view
    - if no-one is assigned, do not change view

--- app/components/assignment-info.js
+++ app/components/assignment-info.js
@@ -1,30 +1,46 @@
 import Ember from 'ember';
 import ResourceInfoComponent from "./resource-info";
+import { storageFor } from 'ember-local-storage';
 
 export default ResourceInfoComponent.extend({
-    persons: 0, // any person who is working on the project
-    currentAssignment: 0,   // current assignment we want to view specifically
     column: 495,    // starting column to check against - remove hard coding later
+    counter: 0,
+    currentAssignment: 0,   // current assignment we want to view specifically
+    peopleAssigned: 0,  // if there are people assigned to the assignment from the specified date
+    persons: 0, // any person who is working on the project
+    settings: storageFor("settings"),
 
     getPeople(org) {
         var self = this;
         org.forEach(function(person){
+            ++self.counter;
             var exists = $.getJSON('http://localhost:3000/file/' + person.get('ad'), function() {})
             .done(function() {
                 var ad = person.get('ad');
-                if(exists.responseJSON) {
+                if(exists.responseJSON) { // has direct reports
                     self.set('persons.' + ad, self.get('store').query('direct', {manager: ad})).then(function(){
-                        //console.log(ad);
-                        self.hasAssignment(person);
+                        --self.counter;
                         self.getPeople(self.get('persons.' + ad));
+                        self.hasAssignment(person);
+                        if(self.counter===0){ self.done(); }
                     });
                 }
-                else{
-                    //console.log(ad);
-                    self.hasAssignment(person);
+                else { // no direct reports
+                    setTimeout(function(){
+                        --self.counter;
+                        if(self.counter===0){ self.done(); }
+                        self.hasAssignment(person);
+                    }, 0)
                 }
             })
-        });
+        })
+    },
+
+    done() {
+        if(this.peopleAssigned){
+            this.set('settings.view', 'timeaway');
+        }
+        this.set('peopleAssigned', 0);
     },
 
     // checks if the person has a specific assignment from a specific date
@@ -37,19 +53,23 @@ export default ResourceInfoComponent.extend({
             })
 
         if(assignment.length){
-            console.log(person.get('ad'))
-            console.log(assignment)
+            this.set('peopleAssigned', ++this.peopleAssigned);
         }
     },
 
     actions: {
         // get vacation of those who are on the project
         viewVacation() {
-            var self = this;
-            this.set('currentAssignment', this.get('assignment.id'));
-            this.set('persons', this.get('store').query('direct', {manager: 'PL145'})).then(function(){
-                self.getPeople(self.persons);
-            });
+            if(this.get('settings.view') === 'timeaway') {
+                this.set('settings.view', 'assignment')
+            }
+            else {
+                var self = this;
+                this.set('currentAssignment', this.get('assignment.id'));
+                this.set('persons', this.get('store').query('direct', {manager: 'PL145'})).then(function(){
+                    self.getPeople(self.persons);
+                })
+            }
         },
 
         select() {
