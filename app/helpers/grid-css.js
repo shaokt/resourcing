18015497b8795894f6730fe0abe1663df7644451
shaokt <shaorobics@gmail.com>
2016-06-24 21:01:58 -0400

    assignment tiles wider in vacation view
    - if viewing employees assigned to assignment, the assignment tiles
    need to fit the daily view
    - this is because the default is the weekly view
    - grid updates to allow for the x-axis placement (value * 5)
    - need to figure out tiles falling out of the daily view

--- app/helpers/grid-css.js
+++ app/helpers/grid-css.js
@@ -3,8 +3,11 @@ import Ember from 'ember';
 // prints out the crid for css - not called unless grid needs to change
 export function gridCss(params/*, hash*/) {
     var gridcss = "";
+    var gridAssignmentInTimeView = "";
     for(var gx = 0; gx <= 9315; gx+=15){ gridcss += '[data-x="' + gx +'"] { left:' + gx + 'px; }' + "\n"; }
-    return gridcss;
+
+    for(var gx = 0; gx <= 9315; gx+=15){ gridAssignmentInTimeView += '[data-x="' + gx +'"] { left:' + gx*5 + 'px; }' + "\n"; }
+    return gridAssignmentInTimeView;
 }
 
 export default Ember.Helper.helper(gridCss);
