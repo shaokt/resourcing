08c56fe369a1f498840ed1e830bdc7b2b6a4c96d
shaokt <shaorobics@gmail.com>
2016-08-07 12:09:18 -0400

    bugfix: width of assignment during teamView
    - now that we aren’t using individual tiles but one block, needed to
    update calculation to display the daily view for all assignments *
    phases

--- app/components/assignment-block.js
+++ app/components/assignment-block.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
+import { storageFor } from 'ember-local-storage';
 
 export default Ember.Component.extend({
     tagName:'div',
     classNames:['tiles'],
+    settings: storageFor("settings"),
     widthCalc: Ember.computed('assignment.w', function() {
         return Ember.String.htmlSafe(this.get('assignment.w'));
+    }),
+    widthCalcTeamView: Ember.computed('assignment.w', function() {
+        return Ember.String.htmlSafe(this.get('assignment.w') * 5);
     }),
     showHandles: Ember.computed('phaseAction', function(){
         return this.get('phaseAction') === '';
