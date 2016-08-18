47faa57ce68aad90afe170355dc006b4c1a7b7f3
shaokt <shaorobics@gmail.com>
2016-08-18 15:26:13 -0400

    WIP - smoother scrolling
    - fixed positioning of items during horizontal scroll
    - absolute positioning of items during vertical scroll

--- app/services/constants.js
+++ app/services/constants.js
@@ -25,6 +25,10 @@ export default Ember.Service.extend({
     webcel:null,        // singleton Webcel object - only one editable instance at a time
     editingRow:false,   // whether a row is being edited or not
     assArray:[],        // array of assignments to view while viewing employees
+    fromV: null,  // original top scroll value before scrolling
+    fromH: null,
+    scrollingV: false,
+    scrollingH: true,
 	padout: function(number) { return (number < 10) ? '0' + number : number; }, // pad single digits to double (for date use)
 
     // save newly added data to store & display saving indicator
@@ -52,10 +56,44 @@ export default Ember.Service.extend({
     },
 
     scrolled: function(minLeft){
+        var self = this;
         var left = $(window).scrollLeft();
-        this.set('leftScroll', left == 0 && this.get('settings.view') == 'timeaway' ? minLeft : left);
+
+        //this.set('leftScroll', left == 0 && this.get('settings.view') == 'timeaway' ? minLeft : left);
+
         $('.calendar').css({left:-left});
         $('.assignmentViewContainer').css({left:-left});
+
+
+        if(this.get('fromH') === null && this.get('fromV') === null) {
+            console.log('default: ' + $(window).scrollTop())
+            this.set('scrollingH', true);
+            //this.set('fromV', parseInt($(window).scrollTop() - 32));
+            this.set('fromH', $(window).scrollLeft())
+        }
+        else if(this.get('fromH') === $(window).scrollLeft()){ // vertical scrolling
+            console.log('v')
+            this.set('scrollingV', true);
+            this.set('scrollingH', false);
+        }
+        else { // horizontal scrolling
+            //console.log('h: ' + this.get('fromH') + " : " + $(window).scrollLeft())
+            this.set('fromH', $(window).scrollLeft())
+            this.set('scrollingH', true);
+            this.set('scrollingV', false);
+        }
+
+        clearTimeout($.data(this, 'scrollTimer'));
+        $.data(this, 'scrollTimer', setTimeout(function() {
+            console.log('stopped')
+            //self.set('fromH', null);
+            self.set('scrollingV', false);
+            self.set('scrollingH', true);
+            var newTop = $(window).scrollTop();
+            newTop = (newTop == 0 ? 32 : (newTop + (newTop > 0 ? 32 : -32)));
+            self.set('fromV', -newTop);
+            self.set('the', $(window).scrollTop())
+        }, 250));
     },
 
     mouseMoved: function(event){
