99ee0726615f4c6656bf89ac6e955e1dff494cf8
shaokt <shaorobics@gmail.com>
2016-10-23 17:19:00 -0400

    added missing ; and $ fix
    replaced $ with Ember.$ and added missing ;

--- app/components/assignment-phase.js
+++ app/components/assignment-phase.js
@@ -12,7 +12,7 @@ export default Ember.Component.extend(KeyDownMixin, {
         return this.get('phaseAction') === '';
     }),
     shiftPhase: function(){
-        return this.get('phaseAction') == 'shift';
+        return this.get('phaseAction') === 'shift';
     }.property('phaseAction'),
 
     /* move the phases around when active
@@ -24,8 +24,8 @@ export default Ember.Component.extend(KeyDownMixin, {
         if(fromBinding && !event.target.type && [32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
             event.preventDefault();
             if(this.get('phaseToShift')){
-                var x = parseInt($(this.get('phaseToShift')).attr('data-x')) + this.get('rowComponent.shiftHorizontal');
-                var y = parseInt($(this.get('phaseToShift')).attr('data-y')) + this.get('rowComponent.shiftVertical');
+                var x = parseInt(Ember.$(this.get('phaseToShift')).attr('data-x')) + this.get('rowComponent.shiftHorizontal');
+                var y = parseInt(Ember.$(this.get('phaseToShift')).attr('data-y')) + this.get('rowComponent.shiftVertical');
 
     			switch (event.keyCode){
                     case 37: { // left
@@ -64,13 +64,13 @@ export default Ember.Component.extend(KeyDownMixin, {
     // update the edited phases so they are in the store upon save
     updatePhases: function(){
         try{
-            $(this.get('phaseToShift'))
+            Ember.$(this.get('phaseToShift'))
                 .removeClass('active')
                 .parent().find('[data-phaselink]').filter(function(){
-                    $(this).removeAttr('data-phaselink');
-                })
+                    Ember.$(this).removeAttr('data-phaselink');
+                });
 
-            var editedPhases = ($(this.get('row')).find('.phases')[0].innerHTML.replace(/<!---->/g, '')).htmlSafe()
+            var editedPhases = (Ember.$(this.get('row')).find('.phases')[0].innerHTML.replace(/<!---->/g, '')).htmlSafe();
             this.set('assignment.phases', editedPhases);
         }catch(e){} // no action taken, nothing to update
     },
@@ -78,8 +78,8 @@ export default Ember.Component.extend(KeyDownMixin, {
     actions: {
         // readies the row for stamping phases onto the project
         setStamp() {
-            this.send('donePhaseAction')
-            this.set('assignment.stampPhase', this.get('label')) // the label to stamp onto the project
+            this.send('donePhaseAction');
+            this.set('assignment.stampPhase', this.get('label')); // the label to stamp onto the project
             this.set('current', event.target);
             this.set('phaseAction', "stamp");
         },
@@ -93,7 +93,7 @@ export default Ember.Component.extend(KeyDownMixin, {
 
         // readies the row for shifting phases around
         shift() {
-            if(this.get('phaseAction') === 'shift') return; // prevents bindKeyDown multiple times
+            if(this.get('phaseAction') === 'shift'){ return; } // prevents bindKeyDown multiple times
             this.set('phaseAction', "shift");
             this.set('current', event.target);
             this.set('rowComponent.breakLink', this.breakLink);
@@ -108,13 +108,13 @@ export default Ember.Component.extend(KeyDownMixin, {
 
         // done current action, reset states
         donePhaseAction(endAll) {
-            $(this.get('current')).prop('checked', false);
+            Ember.$(this.get('current')).prop('checked', false);
             this.get('rowComponent').updateRelatedPhasesPosition();
             this.updatePhases();
             this.unbindKeyDown();
             this.set('phaseToShift', null);
             this.set('assignment.stampPhase', null);
-            endAll ? this.set('phaseAction', "") : 0;
+            if(endAll){ this.set('phaseAction', ""); }
         }
     }// actions
 });
