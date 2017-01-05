e8a0830de14958812d90dd3fb6906110ac64d19e
shaokt <shaorobics@gmail.com>
2017-01-05 16:17:48 -0500

    vacation & personal counters update
    - vacation & personal counters should not be done via CSS
    - this is to take into account 1/2 vacation and 1/2 personal days
    - CSS counters cannot add two increments together
    - updated counter math to run afterRender, otherwise it was null
    - CSS updates

--- app/components/resource-counters.js
+++ app/components/resource-counters.js
@@ -2,5 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     tagName: 'div',
-    classNames: ['counters']
+    classNames: ['counters'],
+
+    vacation: Ember.computed('resource.vacation', function(){
+        return(this.get('resource.vacation'));
+    }),
+
+    vacationToDate: Ember.computed('resource.vacationToDate', function(){
+        return(this.get('resource.vacationToDate'));
+    }),
+
+    personal: Ember.computed('resource.personal', function(){
+        return(this.get('resource.personal'));
+    })
 });
