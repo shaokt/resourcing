87a584df63eed3fd5bbb4c01b2f5abe909b2cbfb
shaokt <shaorobics@gmail.com>
2017-01-14 19:04:06 -0500

    bug fix - viewTeam & calendar, phase customization
    - if the quarter doesn’t start on a Monday, the phases need to be
    shifted to the left to account for a non-Monday start
    - missed quotes around the action

--- app/components/calendar-widget.js
+++ app/components/calendar-widget.js
@@ -167,10 +167,11 @@ export default Ember.Component.extend(WebcelMixin, {
 
     // customize page after calendar loads
     onload() {
+        const cal = Ember.$('.calendar');
         this.constants.webcel = this.Webcel();
         this.set('constants.calWidth', ((this.constants.numDays) * this.constants.DIM));
-        Ember.$('#pageContainer').css({width:this.constants.calWidth});
-        Ember.$('.calendar').css({width:this.constants.calWidth});
+        Ember.$('#pageContainer').css({width:this.constants.calWidth}).attr('data-q1-start', cal.find('.day').attr('data-day-name'));
+        cal.css({width:this.constants.calWidth});
 
         this.calendar = Ember.$('#' + this.attrs.elementId);
         this.highlightToday();
