71d966241e8f81659542eb4b92a3a88baa15f88c
shaokt <shaorobics@gmail.com>
2017-01-15 21:01:21 -0500

    bump phases up when removing rows

--- app/components/assignment-row-management.js
+++ app/components/assignment-row-management.js
@@ -11,6 +11,17 @@ export default Ember.Component.extend({
         return this.get('assignment.rows') || this.get('assignment.rows') > 0;
     }.property('assignment.rows'),
 
+    movePhasesUp: function(num){
+        const newRow = this.constants.DIM*3 + (num*this.constants.DIM);
+        const self = this;
+        Ember.$(this.get('row')).find(`.phases [data-type="tile"]`).each(function(){
+            const y = parseInt(Ember.$(this).attr('data-y'));
+            if(y === newRow){
+                Ember.$(this).attr('data-y',y-self.constants.DIM);
+            }
+        })
+    },
+
     actions: {
         rowAdd(){
             let numRows = this.get('assignment.rows') || 0;
@@ -22,6 +33,7 @@ export default Ember.Component.extend({
         rowMinus(){
             let numRows = this.get('assignment.rows') || 0;
             if(numRows > 0) {
+                this.movePhasesUp(numRows);
                 this.set('assignment.rows', --numRows);
             }
         }
