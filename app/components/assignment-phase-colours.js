75952940f006c53064d4ef1db52fc1081fce0ecd
shaokt <shaorobics@gmail.com>
2017-01-29 15:48:38 -0500

    ability to reset phase colours/tags

--- app/components/assignment-phase-colours.js
+++ app/components/assignment-phase-colours.js
@@ -3,16 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     tagName:'',
     actions: {
+        // add tagging colour on the phase
         updateColour() {
             const colour = Ember.$(event.target).attr('data-colour');
             this.set('assignment.stampCustomize.colour', colour);
-            if(colour === 'A4A4A7') {
-                Ember.$(this.get('assignment.stampCustomize')[0]).removeAttr('data-colour');
-            }
-            else {
-                Ember.$(this.get('assignment.stampCustomize')[0]).attr('data-colour', colour);
-            }
+            Ember.$(this.get('assignment.stampCustomize')[0]).attr('data-colour', colour);
             return false;
+        },
+
+        // remove tagging colour on the phase
+        resetColour() {
+            this.set('assignment.stampCustomize.colour', '');
+            Ember.$(this.get('assignment.stampCustomize')[0]).removeAttr('data-colour');
+            Ember.$('#doneCustomize').focus();
         }
     }
 });
