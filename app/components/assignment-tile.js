3aad78408ccedee4365167b5e0d59cb709d2f35a
shaokt <shaorobics@gmail.com>
2017-02-12 08:26:03 -0500

    clear assignments selected for viewing

--- app/components/assignment-tile.js
+++ app/components/assignment-tile.js
@@ -2,4 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     tagName: '',
+    actions: {
+        // clear all checkboxes indicating which assignments to view
+        clear() {
+            this.get('model').forEach((item)=>{
+                if(Ember.$.inArray(item.id, this.get('constants.assArray')) !== -1){
+                    Ember.$(`#ass-${item.id}-v`).click();
+                }
+            });
+            this.set('constants.assArray', []);
+        }
+    }
 });
