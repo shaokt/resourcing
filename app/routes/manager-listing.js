e27480dc7a788676326ecac68985027efedc3d19
shaokt <shaorobics@gmail.com>
2017-04-09 12:29:21 -0400

    bug fix - calculate next year
    - parseInt needed to compare years
    - manager listing browser title updated
    - styling for “empty file” message

--- app/routes/manager-listing.js
+++ app/routes/manager-listing.js
@@ -7,6 +7,7 @@ export default Ember.Route.extend({
     },
 
     beforeModel: function(transition){
+        document.title = `${transition.queryParams.year} Manager Listing`;
         transition.queryParams.year = transition.queryParams.year || (new Date().getFullYear());
         this.set('year', transition.queryParams.year);
     },
